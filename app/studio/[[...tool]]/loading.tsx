784e394ee311e96a50c2fd9145123597532b768b
Josh <josh@joshrweaver.com>
2025-11-09 23:01:11 +0000

    feat: sanity studio

---
+++ app/studio/[[...tool]]/loading.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+export default function Loading() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        fontSize: '1.5rem',
+      }}
+    >
+      Loading Sanity Studio...
+    </div>
+  )
+}
